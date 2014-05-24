0b6a2a5dd53f37180dc334dd54e6626bfb538fee
Raphael Wimmer <raphael.wimmer@ifi.lmu.de>
2014-05-25 00:14:58 +0200

    fixed wrong names in demo

--- wiimote_demo.py
+++ wiimote_demo.py
@@ -2,7 +2,11 @@
 
 import wiimote
 
-addr, name = wiimote.list()[0]
+raw_input("Press the 'sync' button on the back of your Wiimote Plus\
+           or buttons (1) and (2) on your classic Wiimote. \
+           Press <return> once the Wiimote's LEDs start blinking.")
+
+addr, name = wiimote.find()[0]
 wm = wiimote.connect(addr, name)
 wm.leds[0] = True
 
