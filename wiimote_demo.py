aa54b30939c86b6ed3da19e96cd77c51d647edfe
Raphael Wimmer <raphael.wimmer@ifi.lmu.de>
2014-05-25 01:25:43 +0200

    demo has a light animation

--- wiimote_demo.py
+++ wiimote_demo.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 
 import wiimote
+import time
 
-raw_input("Press the 'sync' button on the back of your Wiimote Plus\
-           or buttons (1) and (2) on your classic Wiimote. \
-           Press <return> once the Wiimote's LEDs start blinking.")
+raw_input("Press the 'sync' button on the back of your Wiimote Plus " +
+          "or buttons (1) and (2) on your classic Wiimote. " +
+          "Press <return> once the Wiimote's LEDs start blinking.")
 
 addr, name = wiimote.find()[0]
 wm = wiimote.connect(addr, name)
-wm.leds[0] = True
+patterns = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
+while True:
+    for p in patterns:
+        wm.leds = p
+        time.sleep(0.1)
 
