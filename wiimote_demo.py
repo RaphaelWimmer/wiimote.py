ec1091f36e461a0bbace20b7fec2031b8e7c6dfc
Raphael Wimmer <raphael.wimmer@ifi.lmu.de>
2014-05-25 18:28:56 +0200

    wiimote_demo.py also accepts only a bt address as parameter

--- wiimote_demo.py
+++ wiimote_demo.py
@@ -3,16 +3,22 @@
 import wiimote
 import time
 import sys
-
-if len(sys.argv) < 3:
-    raw_input("Press the 'sync' button on the back of your Wiimote Plus " +
-          "or buttons (1) and (2) on your classic Wiimote. " +
+    
+raw_input("Press the 'sync' button on the back of your Wiimote Plus " +
+          "or buttons (1) and (2) on your classic Wiimote.\n" +
           "Press <return> once the Wiimote's LEDs start blinking.")
+
+if len(sys.argv) == 1:
     addr, name = wiimote.find()[0]
-else:
+elif len(sys.argv) == 2:
+    addr = sys.argv[1]
+    name = None
+elif len(sys.argv) == 3:
     addr, name = sys.argv[1:3]
 print("Connecting to %s (%s)" % (name, addr))
 wm = wiimote.connect(addr, name)
+
+# Demo Time!
 patterns = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
 while True:
     for p in patterns:
