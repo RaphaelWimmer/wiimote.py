83485138751f7a1b9c55fb024a23e96c6046ebe3
Raphael Wimmer <raphael.wimmer@ifi.lmu.de>
2014-05-25 15:46:03 +0200

    wiimote_demo.py: allow specifying btaddr/model instead of discovery, misc.

--- wiimote_demo.py
+++ wiimote_demo.py
@@ -2,16 +2,20 @@
 
 import wiimote
 import time
+import sys
 
-raw_input("Press the 'sync' button on the back of your Wiimote Plus " +
+if len(sys.argv) < 3:
+    raw_input("Press the 'sync' button on the back of your Wiimote Plus " +
           "or buttons (1) and (2) on your classic Wiimote. " +
           "Press <return> once the Wiimote's LEDs start blinking.")
-
-addr, name = wiimote.find()[0]
+    addr, name = wiimote.find()[0]
+else:
+    addr, name = sys.argv[1:3]
+print("Connecting to %s (%s)" % (name, addr))
 wm = wiimote.connect(addr, name)
 patterns = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
 while True:
     for p in patterns:
         wm.leds = p
-        time.sleep(0.1)
+        time.sleep(0.05)
 
